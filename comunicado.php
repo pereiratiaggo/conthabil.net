dcaf864f0c622ff670ff398ffd812421d647cdce
TIAGO PEREIRA <TIAGO PEREIRA@TIAGO-PC>
2017-02-20 09:15:23 -0300

    criação do comunicado.php e require no index quando comunicado = true.

---
+++ comunicado.php
@@ -0,0 +1,29 @@
+<style type="text/css">
+div.comunicado{
+    z-index: 3;
+    position: fixed;
+    margin-top: 150px;
+    left: 50%;
+    transform: translate(-50%);	
+    max-width: 80%;	
+    vertical-align: middle;
+    text-align: center;
+    padding: 20px;
+}
+</style>
+<div class="alert alert-info alert-dimissible comunicado">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <h2>Comunicado - Feriado dia 28/02/2017</h2>
+    <hr />
+    <p>
+        Informamos, que a Conthábil Assessoria não terá expediente nos dias 27/02 e 28/02/2017, devido ao feriado do Carnaval.<br />
+        <br />
+        Retornamos ao atendimento normal no dia 01/03/2017.<br />
+        <br />
+        Pedimos a gentileza de antecipar seus assuntos.<br />
+        <br />
+        Agradecemos à compreensão.<br />
+        <br />
+        Conthábil Assessoria.
+    </p>
+</div>
--- index.php
+++ index.php
@@ -61,6 +61,12 @@ div.noticias-sm a
 </head>
 
 <body id="page-top">
+<?php
+$comunicado = true;
+if($comunicado){
+    require_once 'comunicado.php';
+}
+?>
 <div class="noticias col-md-4 hidden-xs hidden-sm">
     <h4>
     <a href="http://noticias.conthabil.net" target="_blank">Noticias</a>
