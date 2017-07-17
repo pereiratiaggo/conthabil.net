94bcacdaf18179ce0d9275d12ec3576701dadffe
tiago <tiago@TIAGO-PC>
2017-07-17 17:24:30 -0300

    Inclusao de uma nova fonte de noticias http://www.contabeis.com.br/rss/noticias/

--- noticias.php
+++ noticias.php
@@ -1,29 +1,38 @@
 <?php
+function getHtmlTableFromWordpressRss($rss_address, $show_limit=3){
     $titulo = array();
     $link = array();
     $link_title = array();
     $descricao = array();
     $quantos = 0;
-    $exibir = 3;
+    $exibir = $show_limit;
     $limite_title = 100;
     $limite_descricao = 50;
-    $xml = simplexml_load_file("http://conthabil.net/noticias/index.php/feed/")->channel->item;
+    $xml = simplexml_load_file($rss_address)->channel->item;
     foreach($xml as $item)
     {
         $titulo[] = substr($item->title, 0, $limite_title) . "...";
 	$link[]	= $item->link;
 	$link_title[] = $item->title;
 	$descricao[] = substr($item->description, 0, $limite_descricao)."...";
-	$publicacao[] = date("m/d/Y", strtotime($item->pubDate));
+	$publicacao[] = date("d/m/Y", strtotime($item->pubDate));
 	$quantos++;
     }
 	
+    $html = '';
+    
     for($i = 0; $i < $exibir; $i++)
-    {
+    {        
         if($descricao[$i]!="")
 	{
-            echo "<a href='$link[$i]' title='$link_title[$i]' target='_blank'>$publicacao[$i] - $titulo[$i]</a><br>";
-            echo $descricao[$i]."<br><br>";
+            $html .= "<a href='$link[$i]' title='$link_title[$i]' target='_blank'>$publicacao[$i] - $titulo[$i]</a><br>";
+            $html .= $descricao[$i]."<br><br>";
 	}
     }
+    
+    return $html;
+}
+
+echo getHtmlTableFromWordpressRss("http://www.contabeis.com.br/rss/noticias/", 2);
+echo getHtmlTableFromWordpressRss("http://conthabil.net/noticias/index.php/feed/", 1);
 ?>
